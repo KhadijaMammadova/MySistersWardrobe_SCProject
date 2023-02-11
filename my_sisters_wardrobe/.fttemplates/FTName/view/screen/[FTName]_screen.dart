a28edb6cf522bb6d798b04227dc699ff64980acd
Sena Çelik <sena.celik@miateknoloji.com>
2023-02-11 19:09:17 +0300

    create template

---
+++ my_sisters_wardrobe/.fttemplates/FTName/view/screen/[FTName]_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class <FTName | pascalcase> extends StatelessWidget {
+  const <FTName | pascalcase> ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(child: Text('<FTName | capitalcase> Structure'),),
+    );
+  }
+}
